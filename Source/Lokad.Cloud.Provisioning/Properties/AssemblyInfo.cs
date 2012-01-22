f0d5745ad3d85100179326a679256a1b380e1d5c
Christoph Ruegg <christoph.ruegg@lokad.com>
2012-01-23 00:15:52 +0100

    Cleanup assembly versions and NuGet spec

--- Source/Lokad.Cloud.Provisioning/Properties/AssemblyInfo.cs
+++ Source/Lokad.Cloud.Provisioning/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright (c) Lokad 2009-2011
+﻿#region Copyright (c) Lokad 2009-2012
 // This code is released under the terms of the new BSD licence.
 // URL: http://www.lokad.com/
 #endregion
@@ -7,15 +7,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Lokad.Cloud.Provisioning")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright © Lokad 2011")]
+[assembly: AssemblyCopyright("Copyright © Lokad 2009-2012")]
 [assembly: AssemblyTrademark("")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyCompany("Lokad")]
+[assembly: AssemblyProduct("Lokad.Cloud.Provisioning")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+
 [assembly: Guid("A97DC6D0-E9B5-4BE9-89F3-63D71952B3CA")]
 
-[assembly: InternalsVisibleTo("Lokad.Cloud.Framework.Test")]
+[assembly: InternalsVisibleTo("Lokad.Cloud.Provisioning.Test")]
